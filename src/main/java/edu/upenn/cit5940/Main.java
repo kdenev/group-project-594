58f1278240d622fe74d74ca2326cc3fe3b7765a4
JK77777777 <77685901+JK77777777@users.noreply.github.com>
2026-04-04 15:25:17 +0200

    filepath uses / instead of \ now

--- src/main/java/edu/upenn/cit5940/Main.java
+++ src/main/java/edu/upenn/cit5940/Main.java
@@ -8,7 +8,7 @@ import edu.upenn.cit5940.datamanagement.LoadData;
 public class Main {
     public static void main(String[] args) {
 
-        LoadData dataLoader = new LoadData("src\\main\\java\\edu\\upenn\\cit5940\\datamanagement\\data\\articles_small.csv");
+        LoadData dataLoader = new LoadData("src/main/java/edu/upenn/cit5940/datamanagement/data/articles_small.csv");
         HashMap<String, ArrayList<String>> articles = new HashMap<>(dataLoader.getArticles());
 
         System.out.println(articles.get("6949974693"));
